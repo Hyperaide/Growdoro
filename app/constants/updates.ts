eb4619a741ddca9cd8bd00e2d30d8565fe3c2858
John Paul <johnarpaul@gmail.com>
2025-11-22 21:35:50 +0000

    Update updates.ts

--- app/constants/updates.ts
+++ app/constants/updates.ts
@@ -11,4 +11,28 @@ export const UPDATES = [
     description: "Fixed a bug that would result in no rewards for completed timers when they are cancelled.",
     date: "2025-07-22T19:30:00",
   },
+  {
+    id: 3,
+    title: "New plant: Sunflower",
+    description: "Added a new plant: Sunflower. It's a tall and cheerful plant that follows the sun and brings joy to any garden.",
+    date: "2025-11-22T10:15:00",
+  },
+  {
+    id: 4,
+    title: "Moved to a sprite based rendering system",
+    description: "Moved to a sprite based rendering system to improve performance and reduce lag when viewing gardens with many blocks.",
+    date: "2025-11-22T10:15:00",
+  },
+  {
+    id: 5,
+    title: "Added break timers",
+    description: "You can now choose between focus and break timers to help you stay focused and take breaks.",
+    date: "2025-11-22T10:15:00",
+  },
+  {
+    id: 6,
+    title: "Added night mode",
+    description: "Dark mode with a fun little twist.",
+    date: "2025-11-22T10:15:00",
+  },
 ];
